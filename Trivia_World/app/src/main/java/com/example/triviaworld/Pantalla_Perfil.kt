c6d2dfc3c38dd06585c6f69984b764943ff6d514
XXnikoDarkXX <nicocheto1@gmail.com>
2020-11-16 21:12:10 +0100

    Añadido marco Naranaja posicion Horizontal
    he añadido el marco naranja en modo horizontal

---
+++ Trivia_World/app/src/main/java/com/example/triviaworld/Pantalla_Perfil.kt
@@ -0,0 +1,11 @@
+package com.example.triviaworld
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Pantalla_Perfil : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.pantalla_perfil)
+    }
+}
