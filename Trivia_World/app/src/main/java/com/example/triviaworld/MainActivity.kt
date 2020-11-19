8453d1fa00cb412f24a1324c572bfd3760a8c993
XXnikoDarkXX <nicocheto1@gmail.com>
2020-11-19 20:17:56 +0100

    Perfil: Creacion de Permiso de almacenamiento y Opciones: validacion de campos
    He creado el registro de permisos de almacenamiento en perfil añadiendolo en el manifest y mediante el boton de subir foto que me pida permisos
    Opciones: He creado una validacion de campos por si no hay un campo rellenado me de un mensaje de error

--- Trivia_World/app/src/main/java/com/example/triviaworld/MainActivity.kt
+++ Trivia_World/app/src/main/java/com/example/triviaworld/MainActivity.kt
@@ -1,14 +1,23 @@
 package com.example.triviaworld
 
+import android.Manifest
+import android.Manifest.*
+import android.content.DialogInterface
 import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.EditText
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.app.ActivityCompat.checkSelfPermission
 
 class MainActivity : AppCompatActivity() {
+    companion object val PERMISO_ALMACENAMIENTO=1000;
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.pantalla_principal)
+        setContentView(R.layout.pantalla_perfil)
     }
 
     fun irARedes(view: View) {
@@ -17,7 +26,6 @@ class MainActivity : AppCompatActivity() {
         startActivity(intent)
 
     }
-
 
 
 
