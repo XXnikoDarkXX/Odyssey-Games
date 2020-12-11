de4f5ecbf04802c3a7c8296e22149bfe84076ed6
Mastocito99 <superjav1999@gmail.com>
2020-12-11 13:32:46 +0100

    Cambios Pantallas S y EJ
    Cambio en Pantalla Secundaria y de Eleccion de Juego

--- Trivia_World/app/src/main/java/com/example/triviaworld/Pantalla_Secundaria.kt
+++ Trivia_World/app/src/main/java/com/example/triviaworld/Pantalla_Secundaria.kt
@@ -2,6 +2,10 @@ package com.example.triviaworld
 
 import Adapters.AdapterPreguntas
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -9,7 +13,63 @@ import androidx.recyclerview.widget.RecyclerView
 class Pantalla_Secundaria: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        var bundle: Bundle? = intent.extras
+        var valores:ArrayList<String>? = bundle?.getStringArrayList("tipo")
         setContentView(R.layout.pantalla_secundaria)
+
+        if(valores!=null) {
+            var tipo: String = valores.get(0)
+            val imagen: ImageView =findViewById<ImageView>(R.id.imagen_secundaria)
+            val pregunta: TextView = findViewById<TextView>(R.id.textoPregunta)
+            val respuest1: Button = findViewById<Button>(R.id.respuesta1)
+            val respuest2: Button = findViewById<Button>(R.id.respuesta2)
+            val respuest3: Button = findViewById<Button>(R.id.respuesta3)
+            val respuest4: Button = findViewById<Button>(R.id.respuesta4)
+            when(tipo){
+                "Arte"-> {
+                    imagen.setImageResource(R.drawable.imagen1_arte)
+                    pregunta.text=resources.getString(R.string.pregunta1_arte)
+                    respuest1.text=resources.getString(R.string.respuesta1_arte)
+                    respuest2.text=resources.getString(R.string.respuesta2_arte)
+                    respuest3.text=resources.getString(R.string.respuesta3_arte)
+                    respuest4.text=resources.getString(R.string.respuesta4_arte)
+                }
+                "Historia"-> {
+                    imagen.setImageResource(R.drawable.imagen_prueba)
+                    pregunta.text=resources.getString(R.string.pregunta1_historia)
+                    respuest1.text=resources.getString(R.string.respuesta1_historia)
+                    respuest2.text=resources.getString(R.string.respuesta2_historia)
+                    respuest3.text=resources.getString(R.string.respuesta3_historia)
+                    respuest4.text=resources.getString(R.string.respuesta4_historia)
+                }
+                "Entretenimiento"-> {
+                    imagen.setImageResource(R.drawable.imagen1_entretenimiento)
+                    pregunta.text=resources.getString(R.string.pregunta1_entretenimiento)
+                    respuest1.text=resources.getString(R.string.respuesta1_entretenimiento)
+                    respuest2.text=resources.getString(R.string.respuesta2_entretenimiento)
+                    respuest3.text=resources.getString(R.string.respuesta3_entretenimiento)
+                    respuest4.text=resources.getString(R.string.respuesta4_entretenimiento)
+                }
+                "Ciencia"-> {
+                    imagen.setImageResource(R.drawable.imagen1_ciencias)
+                    pregunta.text=resources.getString(R.string.pregunta1_ciencias)
+                    respuest1.text=resources.getString(R.string.respuesta1_ciencias)
+                    respuest2.text=resources.getString(R.string.respuesta2_ciencias)
+                    respuest3.text=resources.getString(R.string.respuesta3_ciencias)
+                    respuest4.text=resources.getString(R.string.respuesta4_ciencias)
+                }
+                "Geografía"-> {
+                    imagen.setImageResource(R.drawable.imagen1_geografia)
+                    pregunta.text=resources.getString(R.string.pregunta1_geografia)
+                    respuest1.text=resources.getString(R.string.respuesta1_geografia)
+                    respuest2.text=resources.getString(R.string.respuesta2_geografia)
+                    respuest3.text=resources.getString(R.string.respuesta3_geografia)
+                    respuest4.text=resources.getString(R.string.respuesta4_geografia)
+                }
+
+            }
+        }
+
 
     }
 }
