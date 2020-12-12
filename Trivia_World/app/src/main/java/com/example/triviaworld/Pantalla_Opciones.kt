04d54d4a12f12b05551cb72e58c359befae76ae1
XXnikoDarkXX <nicocheto1@gmail.com>
2020-12-12 18:50:16 +0100

    Creacion de foto en perfil
    He quitado el recycleview que no me servia en opciones

--- Trivia_World/app/src/main/java/com/example/triviaworld/Pantalla_Opciones.kt
+++ Trivia_World/app/src/main/java/com/example/triviaworld/Pantalla_Opciones.kt
@@ -1,36 +1,23 @@
 package com.example.triviaworld
 
-import Adapters.AdapterOpciones
 import android.content.DialogInterface
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
-import android.widget.ArrayAdapter
-import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
-import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.children
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class Pantalla_Opciones : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.pantalla_opciones)
 
         val datos:ArrayList<String> = arrayListOf("Usuario","Contraseña Antigua","Contraseña Nueva")
 
-        //hago referencia al recycle
-        val recycleOpciones:RecyclerView = findViewById(R.id.recycleOpciones)
-        //creamos el adapter
-        val adapter:AdapterOpciones = AdapterOpciones(this,datos)
-        //a nuestro recycleview le metemos el adapter creado
-        recycleOpciones.adapter=adapter
-
-        //Establecemos el LayoutManager para que se ocupe de mostrar los datos de la lista
-        recycleOpciones.layoutManager= LinearLayoutManager(this)
 
     }
 
@@ -45,23 +32,12 @@ class Pantalla_Opciones : AppCompatActivity() {
      * En caso de que no se rellene algun campo se validará y dará una alerta
      */
     fun cambiaPass(view: View) {
-        //voy a realizar la validacion de campos
-      //  var user: EditText =findViewById(R.id.cambioUsuario)
-       // var contraseñaAntigua: EditText =findViewById(R.id.passAntigua)
-       // var contraseñaActual: EditText =findViewById(R.id.passNueva)
-        val recycleOpciones:RecyclerView = findViewById(R.id.recycleOpciones)
 
-      /*  var hijos:Sequence<View> =recycleOpciones.children
-        val it:Iterator<View>//Creamos un iterador para iterar el recycleview
-        it=hijos.iterator()
-        var textos:ArrayList<String> = ArrayList<String>();//meteremos en este array los botones
+        var user: EditText =findViewById(R.id.cambioUsuario)
+       var contraseñaAntigua: EditText =findViewById(R.id.passAntigua)
+        var contraseñaActual: EditText =findViewById(R.id.passNueva)
 
-        while(it.hasNext()){
-            textos.add((it.next() as EditText).text.toString())//Metemos todos los botones hijos en la variable botones
-        }
-        Toast.makeText(this,textos.get(0),Toast.LENGTH_LONG).show()
-        Toast.makeText(this,textos.get(1),Toast.LENGTH_LONG).show()
-        Toast.makeText(this,textos.get(2),Toast.LENGTH_LONG).show()
+
 
 
             if (user.text.toString().equals("")||contraseñaActual.text.toString().equals("")||contraseñaAntigua.text.toString().equals("")){
@@ -86,6 +62,6 @@ class Pantalla_Opciones : AppCompatActivity() {
 
                          .show()
              }
-     */
+
     }
 }
--- Trivia_World/app/src/main/java/com/example/triviaworld/Pantalla_Perfil.kt
+++ Trivia_World/app/src/main/java/com/example/triviaworld/Pantalla_Perfil.kt
@@ -1,19 +1,34 @@
 package com.example.triviaworld
 
 import android.Manifest
+import android.app.Activity
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.graphics.Bitmap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.View
+import android.widget.ImageView
 import android.widget.Toast
 import androidx.core.app.ActivityCompat
 
 class Pantalla_Perfil : AppCompatActivity() {
     companion object val PERMISO_ALMACENAMIENTO=1000; //valor constante que nosotros usamos para identificar esa pedida de permiso
+    val REQUEST_CODE = 200
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.pantalla_perfil)
+        lateinit var currentPhotoPath: String
+
+
+
+
+
+
+
+
     }
 
 
@@ -23,37 +38,28 @@ class Pantalla_Perfil : AppCompatActivity() {
      * Pedimos los permisos de almacenamiento en caso de no nos dara un error
      */
     fun enviarFoto(view: View) {
-        //pedimos permiso para poder ejecutar la subida de foto
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)
-                == PackageManager.PERMISSION_GRANTED) {
-            Toast.makeText(this,"Me has concedido permiso", Toast.LENGTH_LONG).show()
-        } else {
-            //si le doy a permiso en negativo me dara este error
-            if (!ActivityCompat.shouldShowRequestPermissionRationale(
-                            this, Manifest.permission.READ_EXTERNAL_STORAGE)) {
-                Toast.makeText(this, R.string.PermisoDenegadoAlmacenamiento, Toast.LENGTH_LONG).show()
-            }
+        if (pedirPermisos()==true) {
+            capturePhoto()
 
-            val permisos:Array<String> = arrayOf<String>(Manifest.permission.READ_EXTERNAL_STORAGE)//metemos el permiso a un array de String
-            ActivityCompat.requestPermissions(this,permisos,PERMISO_ALMACENAMIENTO)
         }
+
 
 
     }
 
 
-    override fun onRequestPermissionsResult(requestCode: Int,permissions: Array<out String>,grantResults: IntArray) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        if(requestCode==PERMISO_ALMACENAMIENTO){
-            if(grantResults.size>0&&grantResults.get(0)== PackageManager.PERMISSION_GRANTED){
-                //lanzamos la actividad
-            }else{
-                //Manejarla como queramos.
-                Toast.makeText(this,R.string.almacenamientoDenegado, Toast.LENGTH_LONG).show()
-            }
-        }
+    /*  override fun onRequestPermissionsResult(requestCode: Int,permissions: Array<out String>,grantResults: IntArray) {
+          super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+          if(requestCode==PERMISO_ALMACENAMIENTO){
+              if(grantResults.size>0&&grantResults.get(0)== PackageManager.PERMISSION_GRANTED){
+                  //lanzamos la actividad
+              }else{
+                  //Manejarla como queramos.
+                  Toast.makeText(this,R.string.permisoDenegado, Toast.LENGTH_LONG).show()
+              }
+          }
 
-    }
+      }*/
 
     /**
      * Funcion para ir a la actividad Principal
@@ -73,6 +79,68 @@ class Pantalla_Perfil : AppCompatActivity() {
         this.startActivity(intent)
 
     }
+
+
+    fun pedirPermisos():Boolean{
+
+        //pedimos permiso para poder ejecutar la subida de foto
+        var comprobacionAlmacenamiento:Boolean
+        var comprobacionCamara:Boolean
+
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)
+                == PackageManager.PERMISSION_GRANTED) {
+            Toast.makeText(this,R.string.permisoAceptado, Toast.LENGTH_LONG).show()
+            comprobacionAlmacenamiento=true
+        } else {
+            //si le doy a permiso en negativo me dara este error
+            if (!ActivityCompat.shouldShowRequestPermissionRationale(
+                            this, Manifest.permission.READ_EXTERNAL_STORAGE)) {
+                Toast.makeText(this, R.string.permisoDenegado, Toast.LENGTH_LONG).show()
+
+            }
+
+            val permisos:Array<String> = arrayOf<String>(Manifest.permission.READ_EXTERNAL_STORAGE)//metemos el permiso a un array de String
+            ActivityCompat.requestPermissions(this,permisos,PERMISO_ALMACENAMIENTO)
+            comprobacionAlmacenamiento=false
+        }
+
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+                == PackageManager.PERMISSION_GRANTED ){
+            comprobacionCamara=true
+        }else{
+            if (!ActivityCompat.shouldShowRequestPermissionRationale(
+                            this,Manifest.permission.CAMERA)) {
+                Toast.makeText(this,R.string.permisoDenegado,
+                        Toast.LENGTH_LONG).show()
+
+            }
+            val permisos:Array<String> = arrayOf<String>(Manifest.permission.CAMERA);
+            ActivityCompat.requestPermissions(this,permisos,PERMISO_ALMACENAMIENTO)
+            comprobacionCamara=false
+        }
+        if (comprobacionAlmacenamiento==true&&comprobacionCamara==true){
+            return true
+        }
+        return false
+    }
+
+    fun capturePhoto() {
+
+        val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        startActivityForResult(cameraIntent, REQUEST_CODE)
+    }
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        var foto: ImageView = findViewById(R.id.avatar)
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == Activity.RESULT_OK && requestCode == REQUEST_CODE && data != null){
+            foto.setImageBitmap(data.extras?.get("data") as Bitmap)
+        }
+    }
+
+
+
 
 
 }
