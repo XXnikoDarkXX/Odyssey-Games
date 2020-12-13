e92f19de216fa13eb60a7c82cdf12aae720744e0
Mastocito99 <superjav1999@gmail.com>
2020-12-13 13:44:48 +0100

    Cambios Pantallas
    Cambio en Adapter Respuestas y Pantalla Secuandaria

--- Trivia_World/app/src/main/java/Adapters/AdapterRespuestas.kt
+++ Trivia_World/app/src/main/java/Adapters/AdapterRespuestas.kt
@@ -2,6 +2,9 @@ package Adapters
 
 import android.app.Activity
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
 import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.example.triviaworld.R
@@ -22,7 +25,8 @@ class AdapterRespuestas(val actividad: Activity, val datos: ArrayList<String>) :
     override fun onBindViewHolder(holder: HolderRespuestas, position: Int) {
 
         holder.botonRespuestas.setText(datos.get(position))
-        Toast.makeText(actividad,"funciona", Toast.LENGTH_LONG).show()
+
+
     }
 
 
--- Trivia_World/app/src/main/java/com/example/triviaworld/Pantalla_Secundaria.kt
+++ Trivia_World/app/src/main/java/com/example/triviaworld/Pantalla_Secundaria.kt
@@ -24,81 +24,78 @@ class Pantalla_Secundaria: AppCompatActivity() {
         val datos: ArrayList<String> = ArrayList<String>()
 
 
-       /* datos.set(0,"hola")
-        datos.set(1,resources.getString(R.string.boton2))
-        datos.set(2,resources.getString(R.string.boton3))
-        datos.set(3,resources.getString(R.string.boton4))*/
 
-        datos.add(resources.getString(R.string.boton1))
+        /*datos.add(resources.getString(R.string.boton1))
         datos.add(resources.getString(R.string.boton2))
         datos.add(resources.getString(R.string.boton3))
-        datos.add(resources.getString(R.string.boton4))
+        datos.add(resources.getString(R.string.boton4))*/
 
 
         //Refenciamos al recyclerView
         val recyclerRespuestas: RecyclerView = findViewById<RecyclerView>(R.id.recyclerSecundaria)
 
         //Creamos el adapter
-        val adapter: AdapterRespuestas = AdapterRespuestas(this,datos)
+        val adapter: AdapterRespuestas
 
         //A nuestro recyclerView le metemos el adapter creado
-        recyclerRespuestas.adapter=adapter
+        /*recyclerRespuestas.adapter=adapter
 
         //Establecemos el LayoutManager para que se ocupe de mostrar los datos de la lista
-        recyclerRespuestas.layoutManager= LinearLayoutManager(this)
+        recyclerRespuestas.layoutManager= LinearLayoutManager(this)*/
 
-        /*if(valores!=null) {
+        if(valores!=null) {
             var tipo: String = valores.get(0)
             val imagen: ImageView =findViewById<ImageView>(R.id.imagen_secundaria)
             val pregunta: TextView = findViewById<TextView>(R.id.textoPregunta)
-            val respuest1: Button = findViewById<Button>(R.id.respuesta1)
-            val respuest2: Button = findViewById<Button>(R.id.respuesta2)
-            val respuest3: Button = findViewById<Button>(R.id.respuesta3)
-            val respuest4: Button = findViewById<Button>(R.id.respuesta4)
             when(tipo){
                 "Arte"-> {
                     imagen.setImageResource(R.drawable.imagen1_arte)
                     pregunta.text=resources.getString(R.string.pregunta1_arte)
-                    respuest1.text=resources.getString(R.string.respuesta1_arte)
-                    respuest2.text=resources.getString(R.string.respuesta2_arte)
-                    respuest3.text=resources.getString(R.string.respuesta3_arte)
-                    respuest4.text=resources.getString(R.string.respuesta4_arte)
+                    datos.add(resources.getString(R.string.respuesta1_arte))
+                    datos.add(resources.getString(R.string.respuesta2_arte))
+                    datos.add(resources.getString(R.string.respuesta3_arte))
+                    datos.add(resources.getString(R.string.respuesta4_arte))
                 }
                 "Historia"-> {
                     imagen.setImageResource(R.drawable.imagen_prueba)
                     pregunta.text=resources.getString(R.string.pregunta1_historia)
-                    respuest1.text=resources.getString(R.string.respuesta1_historia)
-                    respuest2.text=resources.getString(R.string.respuesta2_historia)
-                    respuest3.text=resources.getString(R.string.respuesta3_historia)
-                    respuest4.text=resources.getString(R.string.respuesta4_historia)
+                    datos.add(resources.getString(R.string.respuesta1_historia))
+                    datos.add(resources.getString(R.string.respuesta2_historia))
+                    datos.add(resources.getString(R.string.respuesta3_historia))
+                    datos.add(resources.getString(R.string.respuesta4_historia))
                 }
                 "Entretenimiento"-> {
                     imagen.setImageResource(R.drawable.imagen1_entretenimiento)
                     pregunta.text=resources.getString(R.string.pregunta1_entretenimiento)
-                    respuest1.text=resources.getString(R.string.respuesta1_entretenimiento)
-                    respuest2.text=resources.getString(R.string.respuesta2_entretenimiento)
-                    respuest3.text=resources.getString(R.string.respuesta3_entretenimiento)
-                    respuest4.text=resources.getString(R.string.respuesta4_entretenimiento)
+                    datos.add(resources.getString(R.string.respuesta1_entretenimiento))
+                    datos.add(resources.getString(R.string.respuesta2_entretenimiento))
+                    datos.add(resources.getString(R.string.respuesta3_entretenimiento))
+                    datos.add(resources.getString(R.string.respuesta4_entretenimiento))
                 }
                 "Ciencia"-> {
                     imagen.setImageResource(R.drawable.imagen1_ciencias)
                     pregunta.text=resources.getString(R.string.pregunta1_ciencias)
-                    respuest1.text=resources.getString(R.string.respuesta1_ciencias)
-                    respuest2.text=resources.getString(R.string.respuesta2_ciencias)
-                    respuest3.text=resources.getString(R.string.respuesta3_ciencias)
-                    respuest4.text=resources.getString(R.string.respuesta4_ciencias)
+                    datos.add(resources.getString(R.string.respuesta1_ciencias))
+                    datos.add(resources.getString(R.string.respuesta2_ciencias))
+                    datos.add(resources.getString(R.string.respuesta3_ciencias))
+                    datos.add(resources.getString(R.string.respuesta4_ciencias))
                 }
                 "Geografía"-> {
                     imagen.setImageResource(R.drawable.imagen1_geografia)
                     pregunta.text=resources.getString(R.string.pregunta1_geografia)
-                    respuest1.text=resources.getString(R.string.respuesta1_geografia)
-                    respuest2.text=resources.getString(R.string.respuesta2_geografia)
-                    respuest3.text=resources.getString(R.string.respuesta3_geografia)
-                    respuest4.text=resources.getString(R.string.respuesta4_geografia)
+                    datos.add(resources.getString(R.string.respuesta1_geografia))
+                    datos.add(resources.getString(R.string.respuesta2_geografia))
+                    datos.add(resources.getString(R.string.respuesta3_geografia))
+                    datos.add(resources.getString(R.string.respuesta4_geografia))
                 }
 
+
             }
-        }*/
+
+            adapter = AdapterRespuestas(this,datos)
+            recyclerRespuestas.adapter=adapter
+            recyclerRespuestas.layoutManager= LinearLayoutManager(this)
+        }
 
     }
 }
